3f33c30f9ecda757070a6e6484324ebe42f80c65
ivan-sepulveda <ivan.sepulveda2014@gmail.com>
2018-08-29 17:36:59 -0700

    Added string descriptors of arithmetic operations

--- Arithmetic.java
+++ Arithmetic.java
@@ -8,29 +8,29 @@ public class Arithmetic{
 	float x = 0.5f;
 	float y = 0.25f;
 	// Addition
-	System.out.println(a+b); // a plus b, b plus a not needed
-	System.out.println(a+x); // a plus x
-	System.out.println(a+y); // a plus y
-	System.out.println(b+x); // b plus x
-	System.out.println(b+y); // b plus y
-	System.out.println(x+y); // x plus y
+	System.out.println("a + b = " + (a+b)); // a plus b, b plus a not needed
+	System.out.println("a + x = " + (a+x)); // a plus x
+	System.out.println("a + y = " + (a+y)); // a plus y
+	System.out.println("b + x = " + (b+x)); // b plus x
+	System.out.println("b + y = " + (b+y)); // b plus y
+	System.out.println("x + y = " + (x+y)); // x plus y
 	// Division
 	// Start with all a-operations
-	System.out.println(a/b); // a divided by b
-	System.out.println(a/x); // a divided by x
-	System.out.println(a/y); // a divided by y
+	System.out.println("a / b = " + (a/b)); // a divided by b
+	System.out.println("a / x = " + (a/x)); // a divided by x
+	System.out.println("a / y = " + (a/y)); // a divided by y
 	// All b-operationso
-	System.out.println(b/a); // b divided by a
-	System.out.println(b/x); // b divided by x
-	System.out.println(b/y); // b divided by y
+	System.out.println("b / a = " + (b/a)); // b divided by a
+	System.out.println("b / x = " + (b/x)); // b divided by x
+	System.out.println("b / y = " + (b/y)); // b divided by y
 	// All x-operations
-	System.out.println(x/a); // x divided by a
-	System.out.println(x/b); // x divided by b
-	System.out.println(x/y); // x divided by y
+	System.out.println("x / a = " + (x/a)); // x divided by a
+	System.out.println("x / b = " + (x/b)); // x divided by b
+	System.out.println("x / y = " + (x/y)); // x divided by y
 	// All y-operations
-	System.out.println(y/a); // y divided by a
-	System.out.println(y/b); // y divided by b
-	System.out.println(y/x); // y divided by x
+	System.out.println("y / a = " + (y/a)); // y divided by a
+	System.out.println("y / b = " + (y/b)); // y divided by b
+	System.out.println("y / x = " + (y/x)); // y divided by x
 
 	}
 }
